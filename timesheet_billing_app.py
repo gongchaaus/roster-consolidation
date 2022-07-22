da7c7a9c0bf9755feea7bbb1e7c647642a8ca4e2
eddyAtUrbern <63434918+eddyAtUrbern@users.noreply.github.com>
2022-07-23 03:27:09 +1000

    .

--- timesheet_billing_app.py
+++ timesheet_billing_app.py
@@ -55,7 +55,6 @@ import io
 st.title('Timesheet & Billing')
 
 uploaded_files = st.file_uploader("Choose Files", accept_multiple_files = True)
-st.write(len(uploaded_files))
 # if uploaded_files is not None:
 if len(uploaded_files) > 0:
     ts, bl, ot = calc_timesheets_n_billings(uploaded_files)
